9a5a870d9df54196c6efda439eaab402941365c2
Fuzail <fuzailafsar92@gmail.com>
2023-10-27 23:01:32 +0500

    atm

---
+++ README.md
@@ -0,0 +1,11 @@
+# ATM PROJECT
+
+## Installation
+
+### `npm install`
+
+Install all the dependencies
+
+### `npm start`
+
+Runs the app in the development mode.
--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,99 @@
 import inquirer from "inquirer";
+import { faker } from "@faker-js/faker";
 import chalk from "chalk";
 
-const MAX = 9;
-const MIN = 0;
-
-const getRadomNumber = (max = MAX, min = MIN) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-const randomNumber = getRadomNumber();
-const questions = [
-  {
-    type: "number",
-    name: "guessedNumber",
-    message: `Guess a number between ${MAX} and ${MIN}`,
-    validate: (val: string) => {
-      const num = parseInt(val);
-      if (isNaN(num)) {
-        return "Invalid number";
-      }
-      if (num > MAX || num < MIN) {
-        return "Outof range number";
-      }
-      return true;
-    },
-  },
-];
-
-const { guessedNumber } = await inquirer.prompt(questions);
-
-if (randomNumber === guessedNumber) {
-  console.log(chalk.bold.blueBright("Congratulations! You won."));
-} else {
-  console.log(chalk.bold.redBright("Sorry! You loss."));
+interface User {
+  id: string;
+  pin: number;
+  name: string;
+  accountNumber: string;
+  balance: number;
 }
+
+enum Choices {
+  WITHDRAW = "Withdraw",
+  BALANCE_ENQUIRY = "Balance Inquiry",
+  EXIT = "Exit",
+}
+
+(function () {
+  let isDefaultUserCreated = false;
+  function createRandomUser(): User {
+    let pin = parseInt(faker.finance.pin());
+
+    if (!isDefaultUserCreated) {
+      pin = 1234;
+      isDefaultUserCreated = true;
+    }
+
+    return {
+      id: faker.string.uuid(),
+      pin,
+      name: faker.person.fullName(),
+      accountNumber: faker.finance.accountNumber(),
+      balance: parseInt(faker.finance.amount()),
+    };
+  }
+
+  async function ATM(users: User[]) {
+    try {
+      // User pin
+      const { pin } = await inquirer.prompt({
+        type: "password",
+        name: "pin",
+        message: "Enter your ATM card pin",
+      });
+
+      const user = users.find((user) => user.pin === parseInt(pin));
+      if (!user) throw new Error("Invalid user pin");
+
+      let { name, balance } = user;
+
+      console.log(chalk.bold.blueBright(`Welcome! ${name}`));
+
+      // User's Action
+      const { action } = await inquirer.prompt({
+        type: "list",
+        name: "action",
+        choices: Object.values(Choices),
+        message: "Please select an option",
+      });
+
+      switch (action) {
+        case Choices.WITHDRAW:
+          const { amount } = await inquirer.prompt({
+            type: "number",
+            name: "amount",
+            message: "Enter withdraw amount",
+          });
+          if (amount > balance) throw new Error("Insufficient balance");
+
+          balance -= amount;
+          console.log(chalk.bold.blueBright(`Withdraw amount: ${amount}`));
+          console.log(
+            chalk.bold.blueBright(`Balance after withdraw amount: ${balance}`)
+          );
+          break;
+        case Choices.BALANCE_ENQUIRY:
+          console.log(chalk.bold.blueBright(`Your Balance: ${balance}`));
+          break;
+      }
+      console.log(chalk.bold.blueBright("Thanks for using ATM"));
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(chalk.bold.redBright(error.message));
+      } else if (typeof error === "string") {
+        console.log(chalk.bold.redBright(error));
+      }
+    }
+  }
+
+  function init() {
+    const users: User[] = faker.helpers.multiple(createRandomUser, {
+      count: 5,
+    });
+    ATM(users);
+  }
+
+  return init();
+})();
